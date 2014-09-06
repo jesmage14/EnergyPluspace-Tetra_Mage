aa1e67c0622361e6184567dc6beb85a5f2db0653
jesmage14 <jesmage14@gmail.com>
2014-09-05 23:18:10 -0500

    made hydroelectric turbine create power constantly

--- src/main/java/tetramage/nuclearmayhem/tileentity/TileEntityHydroelectricTurbine.java
+++ src/main/java/tetramage/nuclearmayhem/tileentity/TileEntityHydroelectricTurbine.java
@@ -4,14 +4,20 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityHydroelectricTurbine extends TileEntity {
 	
+	public float rotation = 0;
 	
+	public float maxPower = 10000;
+	public float power = 0;
 	
+	public float powerPerRotation = 0.1F;
 	
-	
-	
-	
-	
-	
+	public void updateEntity()
+	{
+		rotation++;
+		
+		power+=powerPerRotation;
+		if(power > maxPower) power = maxPower;
+	}
 	
 	
 	
